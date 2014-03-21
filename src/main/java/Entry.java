286aad48987c7216121b2aea720e1bb0c127f8e0
Kevin Mulvey <king.feruke@gmail.com>
2014-03-21 08:02:39 -0400

    continue working

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -10,7 +10,7 @@ public class Entry {
         WebDriver driver = new FirefoxDriver();
 
         // And now use this to visit Google
-        driver.get("http://getbootstrap.com/2.3.2/index.html");
+        driver.get("http://getbootstrap.com/2.3.2/getting-started.html");
         HomePage hp = new HomePage(driver);
         hp.getShot();
 
